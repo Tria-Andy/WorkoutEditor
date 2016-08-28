e62c65285757e45873dcd50c79aeaaa0a03c0ba6
Tria-Andy <andy-atech@gmx.net>
2016-08-28 11:29:09 +0200

    Swim Lap Time Adjustment after Time Change #23

--- del_swimlap.h
+++ del_swimlap.h
@@ -18,6 +18,9 @@ public:
         painter->save();
         QFont cFont;
         cFont.setPixelSize(12);
+        QColor lapColor,breakColor;
+        lapColor.setRgb(192,192,192);
+        breakColor.setRgb(128,128,128);
         QRect rect_text(option.rect.x()+2,option.rect.y(), option.rect.width(),option.rect.height());
         if(index.column() == 0)
         {
@@ -25,14 +28,14 @@ public:
             if(index.data(Qt::DisplayRole) == "Break")
             {
                 painter->setPen(Qt::white);
-                painter->fillRect(option.rect,QBrush(QColor(128,128,128)));
-                painter->fillRect(rect_text,QBrush(QColor(128,128,128)));
+                painter->fillRect(option.rect,QBrush(breakColor));
+                painter->fillRect(rect_text,QBrush(breakColor));
             }
             else
             {
                 painter->setPen(Qt::black);
-                painter->fillRect(option.rect,QBrush(QColor(192,192,192)));
-                painter->fillRect(rect_text,QBrush(QColor(192,192,192)));
+                painter->fillRect(option.rect,QBrush(lapColor));
+                painter->fillRect(rect_text,QBrush(lapColor));
             }
         }
         else
@@ -40,14 +43,14 @@ public:
             if(index.data(Qt::DisplayRole).toInt() > 0)
             {
                 painter->setPen(Qt::black);
-                painter->fillRect(option.rect,QBrush(QColor(192,192,192)));
-                painter->fillRect(rect_text,QBrush(QColor(192,192,192)));
+                painter->fillRect(option.rect,QBrush(lapColor));
+                painter->fillRect(rect_text,QBrush(lapColor));
             }
             else
             {
                 painter->setPen(Qt::white);
-                painter->fillRect(option.rect,QBrush(QColor(128,128,128)));
-                painter->fillRect(rect_text,QBrush(QColor(128,128,128)));
+                painter->fillRect(option.rect,QBrush(breakColor));
+                painter->fillRect(rect_text,QBrush(breakColor));
             }
         }
         painter->setFont(cFont);
@@ -90,7 +93,7 @@ public:
         int value = spinBox->value();
         int lapstart = model->data(curr_index,Qt::DisplayRole).toInt();
         double lapSpeed = settings().get_speed(QTime::fromString(settings().set_time(value),"mm:ss"),50,settings().isSwim,false).toDouble();
-        qDebug() << settings().get_sportList();
+
         model->setData(index, value, Qt::EditRole);
         model->setData(new_index,lapstart+value, Qt::EditRole);
         model->setData(speed_index,lapSpeed);
