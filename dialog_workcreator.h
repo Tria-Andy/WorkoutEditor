58cf695eb68b0c9e69401bb4be00300ef2306b9c
Tria-Andy <andy-atech@gmx.net>
2017-05-12 16:52:48 +0200

    Change Button active

--- dialog_workcreator.h
+++ dialog_workcreator.h
@@ -42,8 +42,8 @@ private slots:
     void on_toolButton_delete_clicked();
     void on_lineEdit_workoutname_textChanged(const QString &arg1);
     void on_toolButton_close_clicked();
-
     void on_toolButton_cancel_clicked();
+    void set_controlButtons(bool);
 
 private:
     Ui::Dialog_workCreator *ui;
